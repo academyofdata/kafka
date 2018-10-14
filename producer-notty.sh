e26388e93ea72629662d3a6a4e031259b71565c6
Felix <felix@synnefo.net>
2018-10-14 18:18:49 +0000

    include no TTY versions of consumer and producer

---
+++ producer-notty.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+broker=$(docker ps | grep start-kafka | shuf | head -1 | awk '{print $1}')
+brokerip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $broker)
+echo "executing kafka-console-producer on ${broker} which has address ${brokerip}"
+if (( $# > 1 )); then
+	echo "activating key writes; with ':' separator; write messages as <key>:<message>"
+	args="--property \"parse.key=true\" --property \"key.separator=:\""
+else
+	args=""
+fi
+docker exec -i ${broker} bash -c "kafka-console-producer.sh --broker-list ${brokerip}:9092 ${args} --topic $1"

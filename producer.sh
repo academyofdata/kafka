1f976f8e5c998977ba38a99a86777956269b73db
Felix <felix@synnefo.net>
2018-10-14 16:40:30 +0000

    keyed messages

--- producer.sh
+++ producer.sh
@@ -2,4 +2,10 @@
 broker=$(docker ps | grep start-kafka | shuf | head -1 | awk '{print $1}')
 brokerip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $broker)
 echo "executing kafka-console-producer on ${broker} which has address ${brokerip}"
-docker exec -ti ${broker} bash -c "kafka-console-producer.sh --broker-list ${brokerip}:9092 --topic $1"
+if (( $# > 1 )); then
+	echo "activating key writes; with ':' separator; write messages as <key>:<message>"
+	args="--property \"parse.key=true\" --property \"key.separator=:\""
+else
+	args=""
+fi
+docker exec -ti ${broker} bash -c "kafka-console-producer.sh --broker-list ${brokerip}:9092 ${args} --topic $1"

5d8462bf29ece96eb1cb8a0d48a716ee6c7a81f7
Felix <fixone@users.noreply.github.com>
2019-10-27 17:37:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #install java
-sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install default-jre
+
 #download kafka & zookeeper bundle
 KAFKAVER="2.3.0"
 SCALAVER="2.12"

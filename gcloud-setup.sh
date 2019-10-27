de30637d7474084924c505f5d4b7ad1f952cee0d
Felix <fixone@users.noreply.github.com>
2019-10-27 21:29:50 +0200

    Create gcloud-setup.sh

---
+++ gcloud-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+
+gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "zookeeper=true"
+echo "waiting for machine ${NODE} to boot..."
+sleep 30
+gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/setup.sh | bash -s -- -i 1"
+
+#start the second node
+#get the internal IP of the first node to use as Zookeeper IP
+ZKIP=$(gcloud compute instances list --filter="name=${NODE}" --format="value(networkInterfaces[0].networkIP)")

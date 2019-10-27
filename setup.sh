2bd1325da92d34cf75b701eaaaa2126d72d4ed37
Felix <fixone@users.noreply.github.com>
2019-10-27 17:35:11 +0200

    script to install kafka

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#install java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+#download kafka & zookeeper bundle
+KAFKAVER="2.3.0"
+SCALAVER="2.12"
+WORKDIR="/tmp"
+
+wget -q -O ${WORKDIR}/kafka.tgz https://www-eu.apache.org/dist/kafka/${KAFKAVER}/kafka_${SCALAVER}-${KAFKAVER}.tgz && mkdir -p ${WORKDIR}/kafka && tar -C ${WORKDIR}/kafka --strip-components 1 -xzf ${WORKDIR}/kafka.tgz

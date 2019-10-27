e31442d9d996df06745e179e10c93fbd347f5441
Felix <fixone@users.noreply.github.com>
2019-10-27 19:25:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #install java
 sudo apt-get update
-sudo apt-get install -y default-jre default-jre-headless
+sudo apt-get install -y default-jre openjdk-11-jre
 
 ZKIP=$(hostname --ip-address)
 BRKID=0

d269a8d506a26b99f101fca266a6b4603fcae28a
Felix <fixone@users.noreply.github.com>
2019-10-27 17:44:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,11 @@ KAFKAVER="2.3.0"
 SCALAVER="2.12"
 WORKDIR="/tmp"
 
-wget -q -O ${WORKDIR}/kafka.tgz https://www-eu.apache.org/dist/kafka/${KAFKAVER}/kafka_${SCALAVER}-${KAFKAVER}.tgz && mkdir -p ${WORKDIR}/kafka && tar -C ${WORKDIR}/kafka --strip-components 1 -xzf ${WORKDIR}/kafka.tgz
+echo "getting archive ..."
+wget -q -O ${WORKDIR}/kafka.tgz https://www-eu.apache.org/dist/kafka/${KAFKAVER}/kafka_${SCALAVER}-${KAFKAVER}.tgz 
+mkdir -p ${WORKDIR}/kafka
+echo "expanding..."
+tar -C ${WORKDIR}/kafka --strip-components 1 -xzf ${WORKDIR}/kafka.tgz
+SVRIP=$(hostname --ip-address)
+cd ${WORKDIR}
+

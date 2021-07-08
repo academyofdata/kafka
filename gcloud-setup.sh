a3355f76f48b1ca63eb6f801246fe57d2ac72441
Felix <fixone@users.noreply.github.com>
2021-07-08 10:20:02 +0300

    increase delay before install

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -16,7 +16,7 @@ done
 
 gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "${LABELS}"
 echo "waiting for machine ${NODE} to boot..."
-sleep 30
+sleep 45
 echo "running node setup with broker id ${BRKID} and zookeeper ${ZKIP}"
 gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/setup.sh | bash -s -- -i ${BRKID} -z ${ZKIP}" 
 

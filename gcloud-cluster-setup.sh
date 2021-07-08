8f9b153d0781dd98b1b89f3b27a7c3e0ff7e16bd
Felix <fixone@users.noreply.github.com>
2021-07-08 10:29:49 +0300

    add index var

--- gcloud-cluster-setup.sh
+++ gcloud-cluster-setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-NODE=kafka01
+NODE=kafka0
 ZONE=europe-west3-c
 MACHINE=n1-standard-1
+INDEX=1
 
-wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE} -z ${ZONE} -m ${MACHINE} -i 1 -l "zookeeper=true"
+wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE}${INDEX} -z ${ZONE} -m ${MACHINE} -i ${INDEX} -l "zookeeper=true"
 
 #start the second node
 #get the internal IP of the first node to use as Zookeeper IP
 ZKIP=$(gcloud compute instances list --filter labels.zookeeper=true --format="value(networkInterfaces[0].networkIP)")
 
-NODE=kafka02
+INDEX=2
 
-wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE} -z ${ZONE} -m ${MACHINE} -i 2 -k ${ZKIP}
+wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE}${INDEX} -z ${ZONE} -m ${MACHINE} -i ${INDEX} -k ${ZKIP}
 
-NODE=kafka03
+INDEX=3
 
-wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE} -z ${ZONE} -m ${MACHINE} -i 3 -k ${ZKIP}
+wget -qO- https://raw.githubusercontent.com/academyofdata/kafka/master/gcloud-setup.sh | bash -s -- -n ${NODE}${INDEX} -z ${ZONE} -m ${MACHINE} -i ${INDEX} -k ${ZKIP}

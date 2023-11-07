0697ed27f132dc7004f6b2ba01a81afb5df7bd7c
Felix <fixone@users.noreply.github.com>
2023-11-07 11:08:45 +0200

    change ubuntu ver

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -14,7 +14,7 @@ do
   esac
 done
 
-gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "${LABELS}"
+gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "${LABELS}"
 echo "waiting for machine ${NODE} to boot..."
 sleep 45
 ZKARG=""
